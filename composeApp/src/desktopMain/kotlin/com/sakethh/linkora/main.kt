06e546373824810ba595c372c4ec7d8c63af8e86
sakethpathike <sakethpathike@gmail.com>
2024-12-11 19:48:08 +0530

    desktop: Add maximize/float icon button in the custom decorator

--- composeApp/src/desktopMain/kotlin/com/sakethh/linkora/main.kt
+++ composeApp/src/desktopMain/kotlin/com/sakethh/linkora/main.kt
@@ -5,18 +5,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.window.WindowDraggableArea
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Maximize
 import androidx.compose.material.icons.filled.Minimize
+import androidx.compose.material.icons.outlined.Window
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ApplicationScope
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
@@ -32,7 +39,9 @@ fun main() = application {
     ) {
         LinkoraTheme {
             Scaffold(topBar = {
-                TopDecorator(windowState)
+                WindowDraggableArea {
+                    TopDecorator(windowState)
+                }
             }) {
                 App(modifier = Modifier.padding(it))
             }
@@ -44,6 +53,13 @@ fun main() = application {
 private fun ApplicationScope.TopDecorator(windowState: WindowState) {
     Column {
         Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterEnd) {
+            Text(
+                text = "Linkora",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(start = 15.dp).align(
+                    Alignment.CenterStart
+                ),
+            )
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ) {
@@ -51,6 +67,18 @@ private fun ApplicationScope.TopDecorator(windowState: WindowState) {
                     windowState.isMinimized = true
                 }) {
                     Icon(imageVector = Icons.Default.Minimize, contentDescription = null)
+                }
+                IconButton(onClick = {
+                    if (windowState.placement == WindowPlacement.Fullscreen) {
+                        windowState.placement = WindowPlacement.Floating
+                    } else {
+                        windowState.placement = WindowPlacement.Fullscreen
+                    }
+                }) {
+                    Icon(
+                        imageVector = if (windowState.placement != WindowPlacement.Fullscreen) Icons.Default.Maximize else Icons.Outlined.Window,
+                        contentDescription = null
+                    )
                 }
                 IconButton(onClick = {
                     this@TopDecorator.exitApplication()
