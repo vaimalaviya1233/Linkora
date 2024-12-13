14c1fc9c5a91c0c1d6e235a4cbdc9a9d9501456d
sakethpathike <sakethpathike@gmail.com>
2024-12-13 18:55:31 +0530

    Add routes for section-settings screens

--- composeApp/src/commonMain/kotlin/com/sakethh/linkora/ui/navigation/NavigationRoute.kt
+++ composeApp/src/commonMain/kotlin/com/sakethh/linkora/ui/navigation/NavigationRoute.kt
@@ -27,9 +27,57 @@ sealed interface NavigationRoute {
     }
 
     @Serializable
-    data object SettingsScreen : NavigationRoute {
+    sealed class SettingsScreen : NavigationRoute {
         override fun toString(): String {
             return "Settings"
+        }
+
+        data object ThemeSettingsScreen : SettingsScreen() {
+            override fun toString(): String {
+                return "Theme"
+            }
+        }
+
+        data object GeneralSettingsScreen : SettingsScreen() {
+            override fun toString(): String {
+                return "General"
+            }
+        }
+
+        data object AdvancedSettingsScreen : SettingsScreen() {
+            override fun toString(): String {
+                return "Advanced"
+            }
+        }
+
+        data object LayoutSettingsScreen : SettingsScreen() {
+            override fun toString(): String {
+                return "Layout"
+            }
+        }
+
+        data object LanguageSettingsScreen : SettingsScreen() {
+            override fun toString(): String {
+                return "Language"
+            }
+        }
+
+        data object DataSettingsScreen : SettingsScreen() {
+            override fun toString(): String {
+                return "Data"
+            }
+        }
+
+        data object AboutSettingsScreen : SettingsScreen() {
+            override fun toString(): String {
+                return "About"
+            }
+        }
+
+        data object AcknowledgementSettingsScreen : SettingsScreen() {
+            override fun toString(): String {
+                return "Acknowledgement"
+            }
         }
     }
 }
