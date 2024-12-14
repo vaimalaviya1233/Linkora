9000d8f8977577077923aec61906359fec944fa2
sakethpathike <sakethpathike@gmail.com>
2024-12-14 23:26:24 +0530

    refactor code

--- composeApp/src/commonMain/kotlin/com/sakethh/linkora/ui/screens/collections/CollectionDetailPane.kt
+++ composeApp/src/commonMain/kotlin/com/sakethh/linkora/ui/screens/collections/CollectionDetailPane.kt
@@ -2,10 +2,7 @@ package com.sakethh.linkora.ui.screens.collections
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,7 +13,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.navigation.ThreePaneScaffoldNavigator
 import androidx.compose.runtime.Composable
@@ -30,33 +26,30 @@ fun CollectionDetailPane(
     selectedCollectionInfo: SelectedCollectionInfo,
     paneNavigator: ThreePaneScaffoldNavigator<SelectedCollectionInfo>
 ) {
-    Row(modifier = Modifier.fillMaxSize()) {
-        VerticalDivider()
-        Scaffold(modifier = Modifier.fillMaxSize(),topBar = {
-            Column {
-                TopAppBar(actions = {}, navigationIcon = {
-                    IconButton(onClick = {
-                        paneNavigator.navigateBack()
-                    }) {
-                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
-                    }
-                }, title = {
-                    Text(
-                        text = selectedCollectionInfo.name,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        style = MaterialTheme.typography.titleLarge,
-                        fontSize = 18.sp
-                    )
-                })
-                HorizontalDivider(color = MaterialTheme.colorScheme.outline.copy(0.25f))
-            }
-        }) { padding ->
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        Column {
+            TopAppBar(actions = {}, navigationIcon = {
+                IconButton(onClick = {
+                    paneNavigator.navigateBack()
+                }) {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+                }
+            }, title = {
                 Text(
                     text = selectedCollectionInfo.name,
-                    style = MaterialTheme.typography.titleMedium
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontSize = 18.sp
                 )
-            }
+            })
+            HorizontalDivider(color = MaterialTheme.colorScheme.outline.copy(0.25f))
+        }
+    }) { padding ->
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(
+                text = selectedCollectionInfo.name,
+                style = MaterialTheme.typography.titleMedium
+            )
         }
     }
 }
