893a3a700314018e2c4682abcb69b30e853c2580
sakethpathike <sakethpathike@gmail.com>
2024-12-31 23:40:01 +0530

    android: feat: Add dynamic color support for status and navigation bars

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -28,6 +28,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.accompanist.systemuicontroller)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
--- composeApp/src/androidMain/kotlin/com/sakethh/linkora/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/sakethh/linkora/MainActivity.kt
@@ -5,24 +5,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.sakethh.linkora.common.preferences.AppPreferences
 import com.sakethh.linkora.data.local.LinkoraDataStoreName
 import com.sakethh.linkora.data.local.createDataStore
 import com.sakethh.linkora.data.local.repository.PreferencesImpl
+import com.sakethh.linkora.ui.navigation.Navigation
 import com.sakethh.linkora.ui.screens.settings.SettingsScreenViewModel
 import com.sakethh.linkora.ui.theme.AndroidTypography
 import com.sakethh.linkora.ui.theme.DarkColors
 import com.sakethh.linkora.ui.theme.LightColors
 import com.sakethh.linkora.ui.theme.LinkoraTheme
 import com.sakethh.linkora.ui.utils.genericViewModelFactory
+import com.sakethh.platform
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -83,6 +93,35 @@ class MainActivity : ComponentActivity() {
             LinkoraTheme(
                 typography = AndroidTypography, colorScheme = colors
             ) {
+                val systemUIController = rememberSystemUiController()
+                val platform = platform()
+                val mainRoutes = remember {
+                    listOf(
+                        Navigation.Root.HomeScreen,
+                        Navigation.Root.SearchScreen,
+                        Navigation.Root.CollectionsScreen,
+                        Navigation.Root.SettingsScreen
+                    )
+                }
+                val currentBackStackEntry = navController.currentBackStackEntryAsState()
+                val navigationBarElevation = NavigationBarDefaults.Elevation
+                val rootRoutesColor = colorScheme.surfaceColorAtElevation(
+                    navigationBarElevation
+                )
+                LaunchedEffect(currentBackStackEntry.value) {
+                    systemUIController.setSystemBarsColor(colors.surface)
+                    if (platform == Platform.Android.Mobile) {
+                        systemUIController.setNavigationBarColor(
+                            color = if (mainRoutes.any {
+                                    currentBackStackEntry.value?.destination?.hasRoute(it::class) == true
+                                }) rootRoutesColor else colors.surface
+                        )
+                    } else {
+                        systemUIController.setNavigationBarColor(
+                            color = colors.surface
+                        )
+                    }
+                }
                 Surface {
                     App(
                         navController = navController,
